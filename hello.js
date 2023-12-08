da54939348975775f17530a3fcef5268342557fc
bhh9860 <bhh9860@naver.com>
2023-12-09 06:41:07 +0900

    test

---
+++ hello.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path'); // path 모듈 추가
+var app = express();
+
+app.get('*', function (req, res) {
+  // test.html 보여주기
+  res.sendFile(path.join(__dirname, 'test.html'));
+});
+
+app.listen('8000', function () {
+  console.log('Express App Server: Listening on port 11111!');
+});
