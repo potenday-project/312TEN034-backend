158ef6f70f381341362bc136136b064ae32810e5
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-12 17:56:40 +0900

    set-up view template

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
