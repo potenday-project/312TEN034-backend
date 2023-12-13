879137227de030afc1ebe41cfcc3c4dcfb1362c0
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-13 14:43:16 +0900

    add CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -11,6 +12,8 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
