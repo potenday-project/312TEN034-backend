d2b8e8e78b1c40f0e191f4c568a5073d5604b50d
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-15 01:19:36 +0900

    알람 전체 조회, 새로운 알람 조회 API

---
+++ services/alarmService.js
@@ -0,0 +1,36 @@
+const { alarmModel } = require('../models/alarmModel');
+const { challengeCertificationModel } = require('../models/challengeCertificationModel');
+
+const alarmService = {
+  getAlarms: async ({ memberId }) => {
+    // 유저의 알람 전체 조회 및 읽음 상태 모두 업데이트
+    const alarms = await alarmModel.getAlarms({ memberId });
+    await alarmModel.updateAlarmStatus({ memberId });
+
+    const result = Promise.all(
+      alarms.map(async (alarm) => {
+        const [{ authenticate_image_url }] =
+          await challengeCertificationModel.findAuthenticateImageUrlByChallengeCertificationId({
+            challengeCertificationId: alarm.challenge_certification_id,
+          });
+
+        return {
+          ...alarm,
+          authenticateImageUrl: authenticate_image_url,
+        };
+      })
+    );
+
+    return result;
+  },
+
+  getNewAlarmExist: async ({ memberId }) => {
+    const result = await alarmModel.findNewAlarmExist({ memberId });
+
+    return result;
+  },
+};
+
+module.exports = {
+  alarmService,
+};
