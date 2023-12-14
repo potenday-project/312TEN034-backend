ab2b9aba5b5a3b247667cf664500ffce3dce88c0
bhh9860 <bhh9860@naver.com>
2023-12-14 21:09:48 +0900

    s3 챌린지 인증 요청 전송 조건 추가

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -98,20 +98,66 @@ const challengeController = {
     }
   },
   submitImage: async (req, res) => {
-    const result = await challengeService.submitImage(req, res);
-    if (result.success) {
-      return res.status(200).json({
-        success: true,
-        message: '이미지 업로드에 성공했습니다.',
-        data: {
-          IMG_URL: result.data,
-        },
+    // 이미지 업로드 시도
+    const resultSubmitImage = await challengeService.submitImage(req, res);
+    if (resultSubmitImage.success) {
+      // 이미지 업로드 성공시
+      const memberId = await challengeService.approveChallenge({
+        // 이미지 업로드 성공시 챌린지 인증 테이블 인설트시도
+        memberId: getUserIdFromJwt(req.headers.authorization),
       });
+      const { challengeId } = req.body;
+      const IMG_URL = resultSubmitImage.data;
+
+      // 챌린지 인증 테이블 인설트 하기 전,
+      // 제일 최근 인증이 수락되지 않은 인증(Null)인가?
+      const findNullApproved = await challengeService.findNullApproved({ memberId, challengeId });
+      if (findNullApproved.data[0].result === NULL) {
+        return res.status(400).json({
+          success: false,
+          message: 'NULl인 인증이 있습니다.',
+        });
+      } else {
+        // 제일 최근 인증이 Null이 아니라면(True or False)
+        if (findNullApproved.data[0].result == false) {
+          // 제일 최근 인증이 False라면?
+          const submitResult = await challengeService.submitChallenge({
+            memberId,
+            challengeId,
+            IMG_URL,
+            is_authenticate: 0,
+          });
+          if (submitResult.success) {
+            return res.status(200).json({
+              success: true,
+              message: '이미지 업로드에 성공, 챌린지 인증 쿼리문 성공했습니다',
+              data: { IMG_URL, submitResult },
+            });
+          } else {
+            return res.status(400).json({
+              success: false,
+              message: '이미지 업로드에 성공, 챌린지 인증 쿼리문 실패했습니다',
+              data: { IMG_URL },
+              err: resultSubmitImage.err,
+            });
+          }
+        } else {
+          //제일 최근 인증이 True라면?
+          const submitResult = await challengeService.submitChallenge({
+            memberId,
+            challengeId,
+            IMG_URL,
+            is_authenticate: 1,
+          });
+        }
+
+        // 인설트 성공시
+      }
     } else {
       return res.status(400).json({
         success: false,
         message: '이미지 업로드에 실패했습니다.',
-        err: result.err,
+        err: resultSubmitImage.err,
       });
     }
   },
