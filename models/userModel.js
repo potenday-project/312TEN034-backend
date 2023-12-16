bb3283f4035339d24f65cba5a4be6020d353794a
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-17 04:38:00 +0900

    프로필 상태 조회 API

--- models/userModel.js
+++ models/userModel.js
@@ -45,6 +45,32 @@ const userModel = {
       connection.release();
     }
   },
+
+  getUserStatus: async (id) => {
+    const connection = await pool.getConnection();
+
+    try {
+      try {
+        const [completedCountRows] = await connection.query(
+          `SELECT COUNT(m.id) FROM challenge cT LEFT JOIN challenge_participant cpT ON cT.id = cpT.challenge_id LEFT JOIN member m ON cpT.member_id = m.id LEFT JOIN challenge_certification ccT ON cT.id = ccT.challenge_id WHERE m.id = ? AND cT.challenge_status = 'SUCCESS' AND ccT.participation_count = cT.target_count;`,
+          [id]
+        );
+
+        const [memberCountRows] = await connection.query(
+          `SELECT COUNT(DISTINCT m.id) 'friendCount', COUNT(m.id) 'startedCount' FROM challenge cT RIGHT JOIN (SELECT cT.id FROM challenge cT LEFT JOIN challenge_certification ccT ON cT.id = ccT.challenge_id LEFT JOIN member m ON ccT.member_id = m.id WHERE m.id = ? AND ccT.participation_count = 1 AND ccT.is_authenticate = 1 GROUP BY cT.id) myctT ON cT.id = myctT.id LEFT JOIN challenge_certification ccT ON cT.id = ccT.challenge_id LEFT JOIN member m ON ccT.member_id = m.id WHERE m.id != ? AND ccT.participation_count = 1 AND ccT.is_authenticate = 1;`,
+          [id, id]
+        );
+
+        return [completedCountRows, memberCountRows];
+      } finally {
+        connection.release();
+      }
+
+      return rows;
+    } finally {
+      connection.release();
+    }
+  },
 };
 
 module.exports = {
