04a12052a4182787a61f2401024e13edb2a71e8d
bhh9860 <bhh9860@naver.com>
2023-12-17 03:30:01 +0900

    /challenges/in-progress/:id 수정

--- models/memberModel.js
+++ models/memberModel.js
@@ -6,14 +6,15 @@ const memberModel = {
 
     try {
       const [rows, fields] = await connection.query(
-        'SELECT m.champion, m.id, count(cc.id) count FROM member m LEFT JOIN challenge_certification cc ON m.id = cc.member_id LEFT JOIN challenge c ON cc.challenge_id = c.id WHERE c.id = ? AND cc.is_authenticate = TRUE GROUP BY m.id',
+        'SELECT m.id, m.nickname, m.champion, cc.is_authenticate, cc.participation_count, ch.name AS challenge_name, ch.category, ch.reward FROM member m JOIN (SELECT cp.member_id, MAX(cc.created_at) AS max_created_at FROM challenge_participant cp JOIN challenge_certification cc ON cp.member_id = cc.member_id AND cp.challenge_id = cc.challenge_id WHERE cp.challenge_id IN (SELECT cp_inner.challenge_id FROM challenge_participant cp_inner WHERE cp_inner.member_id IN (SELECT cp_sub.member_id FROM challenge_participant cp_sub WHERE cp_sub.challenge_id = ?)) GROUP BY cp.member_id) AS latest_certification ON m.id = latest_certification.member_id JOIN challenge_certification cc ON latest_certification.member_id = cc.member_id AND latest_certification.max_created_at = cc.created_at JOIN challenge ch ON cc.challenge_id = ch.id;',
         [challengeId]
       );
+      return rows;
+    } catch (err) {
+      return err;
     } finally {
       connection.release();
     }
-
-    return rows;
   },
 };
 
